014047023ffe409a8a1c94916230fb0d77255098
Priyank Desai <priyank@finflock.com>
2023-08-16 17:18:39 +0530

    Static Directory added

--- SalonManagmentSystem/settings.py
+++ SalonManagmentSystem/settings.py
@@ -136,3 +136,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
